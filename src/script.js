b22d6a5fdfdc62bf036a8d91a83312901313d7d6
Elifelete-Cavalcante20 <elif.cav00@gmail.com>
2024-01-06 19:03:14 -0300

    Sexto commit (Ultimos ajustes)

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-let rank = pontoRank ("110", "14")
+let rank = pontoRank (110, 14)
 let nivel = "Ferro"
-const Mensagem = "O Herói tem de saldo de " + rank + " e está no nível de " + nivel + "."
+const Mensagem = "O Herói tem de saldo de " + rank + " e está no nível de "
 
 
 function pontoRank (rankVitoria, rankDerrota){
@@ -8,33 +8,33 @@ function pontoRank (rankVitoria, rankDerrota){
     return result
 }
 
-switch (rank){
-    case rank <= "10":
-        console.log (Mensagem)
+switch (true){
+    case rank <= 10:
+        console.log (Mensagem + nivel + ".")
         break;
-    case rank >= "11" || rank <= "20": 
+    case rank >= 11 && rank <= 20: 
         nivel = "Bronze"
-        console.log (Mensagem)
+        console.log (Mensagem + nivel + ".")
         break;
-    case rank >= "21" || rank <= "50":
+    case rank >= 21 && rank <= 50:
         nivel = "Prata"
-        console.log(Mensagem)
+        console.log(Mensagem + nivel + ".")
         break;
-    case rank >= "51" || rank <= "80": 
+    case rank >= 51 && rank <= 80: 
         nivel = "Ouro"
-        console.log(Mensagem)
+        console.log(Mensagem + nivel + ".")
         break;
-    case rank >= "81" || rank <= "90":
+    case rank >= 81 && rank <= 90:
         nivel = "Diamante"
-        console.log(Mensagem)
+        console.log(Mensagem + nivel + ".")
         break;
-    case rank >= "91" || rank <= "100":
+    case rank >= 91 && rank <= 100:
         nivel = "Lendário"
-        console.log(Mensagem)
+        console.log(Mensagem + nivel + ".")
         break;
-    case rank >= "101": 
+    case rank >= 101: 
         nivel = "Imortal"
-        console.log(Mensagem)
+        console.log(Mensagem + nivel + ".")
         break;
 }
 
